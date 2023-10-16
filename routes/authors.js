bea58fe3b3427291b3297fc903ebd1078e0517ec
AlexMaderaP <alejandro.maderap@gmail.com>
2023-10-16 12:47:41 -0600

    Adding header to index

--- routes/authors.js
+++ routes/authors.js
@@ -1,22 +1,20 @@
-const express = require('express');
-const { route } = require('.');
+const express = require("express");
+const { route } = require(".");
 const router = express.Router();
 
 // All Authors Route
-router.get('/', (req,res) => {
-    res.render('authors/index');
+router.get("/", (req, res) => {
+  res.render("authors/index");
 });
 
 // New Author Route
-router.get('/new', (req, res) => {
-    res.render('authors/new')
+router.get("/new", (req, res) => {
+  res.render("authors/new");
 });
 
 // Create Author Route
-router.post('/', (req, res) => {
-    res.send('Create');
+router.post("/", (req, res) => {
+  res.send("Create");
 });
-
-
 
 module.exports = router;
